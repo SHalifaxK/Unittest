d1672685532ea1a4e55d0be3288670987df662d9
SHalifaxK <34966355+SHalifaxK@users.noreply.github.com>
2018-01-24 13:02:32 +0200

    Create unittest_vs_pytest.py

---
+++ unittest_vs_pytest.py
@@ -0,0 +1,27 @@
+#Using Unittest
+
+import unittest
+
+def upper_reverse(text):
+    return ''.join(reversed(text.upper()))
+	
+class TestUpperReversed(unittest.TestCase):
+    def test_upper_reversed(self):
+	    self.assertEqual(upper_reverse('hello'), 'OLLEH')
+
+if __name__=='__main__':
+    unittest.main()
+		
+'''		
+#Using pytest
+
+#no import needed
+
+def upper_reverse(text):
+    return ''.join(reversed(text.upper())
+
+#No need to wrap test case into a class
+def test_upper_reversed():
+	assert upper_reverse('hello') == 'OLLEH'	
+	
+'''
