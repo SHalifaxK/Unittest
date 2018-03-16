c158da899d8a1e4e23da190c3e9e1114dbac6fef
SHalifaxK <34966355+SHalifaxK@users.noreply.github.com>
2018-03-16 10:06:20 +0200

    Create Rconv.py

---
+++ Rconv.py
@@ -0,0 +1,12 @@
+class RomanNumer(object):
+
+    def __init__(self):
+        self.digit_map = {"M":1000, "D":500, "C":100, "L":50, "X":10, "V":5, "I":1}
+        
+    def convert_to_decimal(self, num):
+        self.num = num;
+        val = 0
+        for m in self.num:
+            val += self.digit_map[m]
+        print(val)
+        return val
